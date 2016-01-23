a842e0ab0f0c5da9181ab8f4e0c3849b30447905
angelab <a0002730@unet.univie.ac.at>
2016-01-23 23:34:08 +0100

    cache homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
+## test matrices:
+## ma <- matrix(c(2,5,1,3),nrow=2,ncol=2,byrow=TRUE)
+## ma <- matrix(c(1,2,2,3),nrow=2,ncol=2,byrow=TRUE)
+## WARNING:use only invertible matrices, i.e. det(A) != 0
+## this should be tested, but I don't have time at the moment
+##
+## usage:
+## ta <- makeCacheMatrix(ma)
+## cacheSolve(ta)
+## cacheSolve(ta)
 
-## Write a short comment describing this function
-
+## function creates matrix and caches inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    B <- NULL
+    #  A <<- x
+    set <- function(y){
+        #     A <<- y
+        x <<- y
+        B <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) B <<- solve
+    getsolve <- function() B
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## function: actual computation of inverse matrix returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    B <- x$getsolve()
+    if(!is.null(B)) {
+        message("getting cached data")
+        return(B)
+    }
+    data <- x$get()
+    B <- solve(data, ...)
+    x$setsolve(B)
+    B
 }
